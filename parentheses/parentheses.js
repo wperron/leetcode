9851b058043505df7a604242a5e20b78f92f0da9
wperron <william.perron@live.ca>
2020-02-27 11:14:29 -0500

    improved solution for the generation of parentheses

--- parentheses/parentheses.js
+++ parentheses/parentheses.js
@@ -18,16 +18,38 @@ function valid(s) {
 
 // find all paths through graph
 function generate(n) {
-    let set = ['('] // all combinations must start with an opened bracket
-    for (let i = 0; i < (n * 2) - 1; i++) {
+    let set = ['(']
+    let len = 1
+    while (len < n * 2) { // keep going while permutations are less than twice the limit
         let tmp = []
-        for (let j = 0; j < set.length; j++) {
-            tmp.push(set[j] + ')')
-            tmp.push(set[j] + '(')
+        for (let i = 0; i < set.length; i++) { // for every known path
+            let curr = set[i]
+            let groups = 0  // number of logical groups, ex: ()(()) -> 3 groups
+            let stack  = [] // stack of opened groups
+            for (let j = 0; j < curr.length; j++) {
+                if (curr[j] === '(') {
+                    groups++
+                    stack.push(curr[j])
+                }
+                else {
+                    stack.pop()
+                }
+            }
+
+            if (groups === n) {
+                tmp.push(curr + ')')
+            }
+            else if (stack.length > 0) {
+                tmp.push(curr + '(', curr + ')')
+            }
+            else {
+                tmp.push(curr + '(')
+            }
+            len = curr.length + 1
         }
         set = tmp
     }
-    return set.filter(s => valid(s))
+    return set
 }
 
 module.exports = {
